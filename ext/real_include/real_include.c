e9e3c98400c1ecefc4ff9bf35b005d6065ec3855
John Mair <jrmair@gmail.com>
2010-10-04 16:04:24 +1300

    added bacon tests

--- ext/real_include/real_include.c
+++ ext/real_include/real_include.c
@@ -8,8 +8,6 @@
 #  define OBJ_UNTRUSTED OBJ_TAINTED
 #endif
 
-
-
 #define FALSE 0
 #define TRUE 1
 
---
+++ test/test.rb
@@ -0,0 +1,77 @@
+require 'rubygems'
+require '../lib/real_include'
+require 'bacon'
+
+describe 'Including a module into a class using real_include' do
+  before do
+    @m = Module.new {
+      def self.class_method
+        :class_method
+      end
+
+      def instance_method
+        :instance_method
+      end
+    }
+
+    @c = Class.new
+
+    @c.send(:real_include, @m)
+  end
+
+  it 'should make class methods accessible to class' do
+    @c.class_method.should.equal :class_method
+  end
+
+  it 'should make instance methods accessible to instances of the class' do
+    obj = @c.new
+    obj.instance_method.should.equal :instance_method
+  end
+end
+
+    
+describe 'Including a module into a module and then into a class using real_include' do
+    before do
+    @m1 = Module.new {
+      def self.class_method1
+        :class_method1
+      end
+
+      def instance_method1
+        :instance_method1
+      end
+    }
+
+    @m2 = Module.new {
+      def self.class_method2
+        :class_method2
+      end
+
+      def instance_method2
+        :instance_method2
+      end
+    }
+
+    @m2.send(:real_include, @m1)
+
+    @c = Class.new
+
+    @c.send(:real_include, @m2)
+  end
+
+  it 'should make class methods on m1 accessible to m2' do
+    @m2.class_method1.should.equal :class_method1
+  end
+
+  it 'should make class methods on modules m1 and m2 accessible to class' do
+    @c.class_method1.should.equal :class_method1
+    @c.class_method2.should.equal :class_method2
+  end
+
+  it 'should make instance methods on modules m1 and m2 accessible to instances of class' do
+    obj = @c.new
+
+    obj.instance_method1.should.equal :instance_method1
+    obj.instance_method2.should.equal :instance_method2
+  end
+end
